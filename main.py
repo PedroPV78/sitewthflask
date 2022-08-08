bebf0383d5ce194f0af98fc7befa30ba76de91c4
PedroPV78 <64570217+PedroPV78@users.noreply.github.com>
2022-08-08 06:04:02 -0300

    tirei todo o sql pra rodar sem

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import base64
 from flask import *
-import mysql.connector
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="Pv831842@",
-    database="users"
-)
-
-mycursor = db.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS users")
 
 
 app = Flask(__name__)
@@ -30,9 +18,7 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         if not len(password) == 0:
-            mycursor.execute(
-                "SELECT * FROM loginData WHERE login = '" + username + "' AND senha = '" + password + "'")
-            if mycursor.fetchone():
+            if username == 'admin' && password == 'admin':
                 res = make_response(redirect(url_for("home")))
                 res.set_cookie("login", base64.b64encode(username.encode('ascii')))
                 return res
@@ -48,31 +34,29 @@ def login():
 def logado():
     return render_template("logado.html")
 
-@app.route('/register', methods=["GET", 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form["username"]
-        password = request.form['password']
-        mycursor.execute("SELECT * FROM loginData WHERE login = '" + username + "'")
-        if mycursor.fetchone():
-            return render_template("register.html", existe=True)
+# @app.route('/register', methods=["GET", 'POST'])
+# def register():
+#     if request.method == 'POST':
+#         username = request.form["username"]
+#         password = request.form['password']
+#         mycursor.execute("SELECT * FROM loginData WHERE login = '" + username + "'")
+#         if mycursor.fetchone():
+#             return render_template("register.html", existe=True)
 
-        sql = """INSERT INTO loginInfo(
-        login, senha)
-        VALUES ('%s', '%s')""" % (username, password)
-        mycursor.execute(sql)
-        db.commit()
-        res = make_response(render_template("register.html", login=True))
-        res.set_cookie("login", base64.b64encode(username.encode('ascii')))
-        return res
-    return render_template('register.html')
+#         sql = """INSERT INTO loginInfo(
+#         login, senha)
+#         VALUES ('%s', '%s')""" % (username, password)
+#         mycursor.execute(sql)
+#         db.commit()
+#         res = make_response(render_template("register.html", login=True))
+#         res.set_cookie("login", base64.b64encode(username.encode('ascii')))
+#         return res
+#     return render_template('register.html')
 
 
 @app.route("/edit", methods=["GET", "POST"])
 def editdiario():
-    mycursor.execute("SELECT * FROM posts")
-    posts = mycursor.fetchall()
-    return render_template("editdiario.html", aaaa=len(posts))
+    return render_template("editdiario.html")
 
 
 if __name__ == "__main__":
