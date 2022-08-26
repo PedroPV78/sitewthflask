14ec7affe63b45b10e22f01071ae5f3065bffd60
PedroPV78 <64570217+PedroPV78@users.noreply.github.com>
2022-08-26 16:27:14 -0300

    asd

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            return 'foi'
     return '''
     <!doctype html>
     <title>Upload new File</title>
