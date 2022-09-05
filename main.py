b95f0a8a74c1725e72e246b3ab608ae1c11695e3
deluxePV <ppvverissimo@gmail.com>
2022-09-05 16:08:15 +0000

    i

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def verPerfil():
     mycursor.execute(f"SELECT nomeReal from loginData WHERE login='{base64.b64decode(cookie).decode('utf-8')}'")
     for x in mycursor:
         coiso = x
-	print(coiso))
+        print(coiso)
     return render_template("verPerfil.html")
 
 
